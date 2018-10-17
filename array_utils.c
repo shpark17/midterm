7fbcf61123a1dfcf75354c84dc1bca4ba95d7c37
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-10-17 17:34:20 -0500

    Update array_utils.c

--- array_utils.c
+++ array_utils.c
@@ -3,12 +3,13 @@
 #include<math.h>
 #include"array_utils.h"
 
-int main(int argc char **argv)
+int main(int argc char **argv) {
 
 
-int dotProduct(const int *a, const int *b, int n, int *result)
-  int result = 0;
-  for(i=0; i<n; i++) {
-    result += a[i]*b[i];
-    return result;
-  }
+  int dotProduct(const int *a, const int *b, int n, int *result)
+    int result = 0;
+    for(i=0; i<n; i++) {
+      result += a[i]*b[i];
+      return result;
+    }
+  printf("The dot product of the two arrays are %d\n", result)
