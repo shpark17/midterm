b10b449d10e4a0d549260d008715e6b475eaf747
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-10-17 18:18:15 -0500

    Rename array_utils.c to array_utils1.c

---
+++ array_utils1.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include"array_utils.h"
+
+
+int main(int argc char **argv) {
+
+
+
+int * append(const int *a, int n, const int *b, int m);
+  char array[]
+  strcpy(arr[i], a);
+  strcat(arr[i], "");
+  strcat(arr[i], b);
